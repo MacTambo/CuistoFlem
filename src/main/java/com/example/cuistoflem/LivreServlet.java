4f019c52df0ef28e68c125e9f26b786601fcb68e
MacTambo <58809620+MacTambo@users.noreply.github.com>
2022-02-16 11:36:53 +0100

    added servlets and jsp files

---
+++ src/main/java/com/example/cuistoflem/LivreServlet.java
@@ -0,0 +1,20 @@
+package com.example.cuistoflem;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+
+@WebServlet(name = "Livre Servlet", value = "/Livre Servlet")
+public class LivreServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
